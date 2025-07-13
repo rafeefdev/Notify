0d30eb2f2b62eabae8ce5a143762bebd5fc34e6e
rafeefdev <rafeef.dev03@gmail.com>
2025-07-13 13:17:20 +0700

    add copyWith method

--- lib/core/note_model.dart
+++ lib/core/note_model.dart
@@ -1,3 +1,5 @@
+import 'package:uuid/uuid.dart';
+
 class Note {
   String noteID;
   String title;
@@ -5,5 +7,15 @@ class Note {
   DateTime timeStamp;
 
   Note({required this.noteID, required this.title, required this.content})
-    : timeStamp = DateTime.timestamp();
+    : timeStamp = DateTime.timestamp() {
+    noteID = Uuid().v4();
+  }
+
+  Note copyWith({String? newID, String? newTitle, String? newContent}) {
+    return Note(
+      noteID: newID ?? noteID,
+      title: newTitle ?? title,
+      content: newContent ?? content,
+    );
+  }
 }
